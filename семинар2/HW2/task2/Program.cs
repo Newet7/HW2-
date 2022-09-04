2f5ff70797bc2e77153249ddc198e70781b3cd5b
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-04 17:20:14 +0300

    task2

--- семинар2/HW2/task2/Program.cs
+++ семинар2/HW2/task2/Program.cs
@@ -1,10 +1,38 @@
-﻿int start = 10;
-int stop = 10;
+﻿//Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+int start = 1;
+int stop = 65536;
+int reminder = 0;
 
 int value = new Random().Next(start, stop+1);
 Console.WriteLine(value);
 
-if (value % 10 == 0) {
+while (value / 100 == 0) {
     Console.Write("третей цифры нет");
+    break;
 }
-
+while (value <= 999){
+    
+    reminder = (value % 10);
+    //Console.WriteLine(value % 10);
+    break;
+}
+while (value >= 1000){
+    
+    reminder = ((value % 100)/10);
+    //Console.WriteLine((value % 100)/10);
+    break;
+}
+while (value >= 10000){
+    
+    reminder = ((value % 1000)/100);
+    //Console.WriteLine((value % 1000)/100);
+    break;
+}
+Console.WriteLine(reminder);
