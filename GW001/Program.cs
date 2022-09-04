f948ccbd2d5cde5d878c62ca776c14d8ad387969
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-04 14:40:12 +0300

    gitignore

---
+++ GW001/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int reminder = 0;
+
+if (a % b ==0)
+{
+    Console.Write("b кратно a");
+}
+
+else{
+    reminder = a%b;
+    Console.WriteLine("не кратно, остаток " + reminder);
+}
