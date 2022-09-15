6b8ada33685ab535920c29e5c4830df959f31dd3
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-15 21:04:55 +0300

    seminar3 Ex2

---
+++ Семинар3/Example002/Program.cs
@@ -0,0 +1,22 @@
+﻿//1. Create array 
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+//2. Заполнение массива
+//int len = array.Length;
+int index = 0;
+while(index < N)
+{
+    array[index] = new Random().Next(1, 10+1);
+    Console.Write(array[index] + " ");
+    index ++;
+}
+
+//3. Avarage (mean) среднее арифметическое чисел
+int sum = 0;
+for (int i=0; i<N; i++)
+{
+    sum += array[i];
+}
+double mean = sum / N;
+Console.WriteLine(mean);
