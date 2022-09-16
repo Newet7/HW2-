225cf45a546512e9e183efd23deca8ec406fc156
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-16 20:46:51 +0300

    HW1

---
+++ Семинар3/HW/ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿// принимает на вход 5-ти значное число
+// проверяет его на полиндромность
+
+Console.Write("Пожалуйста введите пятизначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = N;
+int result = 0;
+
+while (N > 0)
+{
+    result *= 10;
+    result += N % 10;  
+    N /= 10; 
+
+}
+
+if (result==num)
+{
+    Console.WriteLine("да");
+    
+}
+else
+{
+    Console.WriteLine("No");
+}
+
