9c824345430274120f42ff29964c7cf6ebd8eb10
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-16 21:07:04 +0300

    HW3

---
+++ Семинар3/HW/ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N
+// и заполняет массив кубами чисел от 1 до N. полученный массив вывести на экран
+
+Console.Write("Пожалуйста введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+int index = 0;
+int num = 1;
+while(index < N)
+{
+    array[index] = num * num * num;
+    Console.Write(array[index] + " ");
+    num ++;
+    index ++;
+}
