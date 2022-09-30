c91909f5be087a0410bfd660c1e887387307831a
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-30 21:10:22 +0300

    factorial2

---
+++ Семинар6/Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Рекурсия
+
+int get_factorial(int x)
+{
+    Console.WriteLine(x); // вывод 4 3 2 1
+    if (x==1)
+    {
+        Console.WriteLine("IF");
+        return 1;
+    }
+    else
+    {
+        //Console.WriteLine("HERE:" + x);
+        //return x*get_factorial(x-1);
+        
+        int tmp = get_factorial(x-1);
+        Console.WriteLine("reverse:" + tmp);
+        return x*tmp;
+
+    }
+}
+
+get_factorial(4);  
---
+++ Семинар6/Ex02Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] arr = new int []{1,2,3,4,5};
+
+for (int i=0; i<arr.Length/2; i++)
+{
+    int tmp = arr[i];
+    arr[i] = arr[arr.Length - (i+1)];
+    arr[arr.Length - (i+1)] = tmp;
+}
+Console.Write("END");
+
