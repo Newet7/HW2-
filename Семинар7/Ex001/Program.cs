46ee28f2c1ef7204ecb9093b158375bd35777c8b
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-10-08 12:18:20 +0300

    S7Ex001

---
+++ Семинар7/Ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿//Случайные целые числа
+
+int strsCount = Convert.ToInt32(Console.ReadLine());
+int columnsCount = Convert.ToInt32(Console.ReadLine());
+
+//1 allocate memory for array
+int [,] twoDimArray = new int [strsCount,columnsCount];
+//2 
+Random rnd = new Random();
+
+for (int i = 0, j = 0; i < strsCount && j < columnsCount; i++, j++)
+{
+    twoDimArray[i,j] = rnd.Next(1, 100);
+    Console.WriteLine("\t", twoDimArray[i,j]);
+}
+Console.WriteLine();
